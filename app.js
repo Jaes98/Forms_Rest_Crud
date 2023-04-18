445f528e8ea90be95375a149a52d6d9f147aaaec
Jaes98 <marcus.jaes@gmail.com>
2023-04-18 09:52:46 +0200

    data fetched fra firebase

--- app.js
+++ app.js
@@ -2,6 +2,28 @@
 
 window.addEventListener("load", initApp);
 
+const endpoint = "https://testings-78997-default-rtdb.firebaseio.com"
+
 function initApp() {
     console.log("Javascript is running!");
+    getPosts();
+}
+async function getPosts() {
+  const response = await fetch(`${endpoint}/posts.json`); //posts.json is the data ressource
+  const data = await response.json();
+  const posts = preparePostData(data);
+  console.log(data);
+  return posts;
+}
+
+function preparePostData(dataObject) {
+    const postArray = [];
+    for (const key in dataObject) {
+      const post = dataObject[key];
+      post.id = key
+      console.log(post);
+      postArray.push(post);
+    }
+    console.log(postArray);
+    return postArray;
 }
